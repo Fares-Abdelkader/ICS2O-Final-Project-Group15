6a8e6e47c38890d8aed8df4f7071c8b845ba903d
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-06-16 16:30:18 +0000

    added favicon

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-// 
+// Copyright (c) 2020 Mr. Coxall All rights reserved
+//
+// Created by: Yuri G
+// Created on: June 2023
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function coinFlip() {
+    // this function shows if the user got heads or tails after flipping a coin
+    
+}
