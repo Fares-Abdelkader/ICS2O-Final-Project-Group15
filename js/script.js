28437d3dbb731d934c79e34c2adae4b29b3f321b
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-06-19 16:50:03 +0000

    added storyboard changed formatting

--- js/script.js
+++ js/script.js
@@ -7,18 +7,20 @@
 "use strict"
 
 function coinFlip() {
-    // this function shows the outcome of a coin flip
-    const flip = Math.random()
-    if (flip < 0.5) {
-        document.getElementById('results').innerHTML = "You landed on heads!"
-    } else {
-        document.getElementById('results').innerHTML = "You landed on tails!"
-}
+  // this function shows the outcome of a coin flip
+  const flip = Math.random()
+  if (flip < 0.5) {
+    document.getElementById("results").innerHTML = "You landed on heads!"
+  } else {
+    document.getElementById("results").innerHTML = "You landed on tails!"
+  }
 
-// show image of heads or tails
-if (flip < 0.5) {
-    document.getElementById('coin').innerHTML = "<img src='images/heads.png' alt='heads' height='200' width='200'>"
-} else {
-    document.getElementById('coin').innerHTML = "<img src='images/tails.png' alt='tails' height='200' width='200'>"
-}
+  // show image of heads or tails
+  if (flip < 0.5) {
+    document.getElementById("coin").innerHTML =
+      "<img src='images/heads.png' alt='heads' height='200' width='200'>"
+  } else {
+    document.getElementById("coin").innerHTML =
+      "<img src='images/tails.png' alt='tails' height='200' width='200'>"
+  }
 }
